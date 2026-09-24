4352ea8c2978d69d1789cd4736a396dedcd523a9
Yuhsensei <giahuy8607@gmail.com>
2026-09-24 13:42:12 +0700

    sua lan cuoi

--- TicketList.h
+++ TicketList.h
@@ -16,11 +16,14 @@ public:
     void clear();
 
     TicketNode* getHead() const;
-    void addTicket(const std::string& ticketID, const std::string& trainCode, int carriageNo, int seatNo, int fromID, int toID);
+    void addTicket(const std::string& ticketID, const std::string& trainCode, 
+                    int carriageNo, int seatNo, int fromID, int toID);
     TicketNode* findTicketByID(const std::string& ticketID) const;
     bool cancelTicket(const std::string& ticketID);
     void displayAllTickets() const; 
-    bool isSeatAvailable(StationList* stationList, const std::string& trainCode, int carriageNo, int seatNo, int newFromID, int newToID) const;
-    void displaySeatMap(StationList* stationList, const std::string& trainCode, int carriageNo, int totalSeats, int fromID, int toID) const;
+    bool isSeatAvailable(StationList* stationList, const std::string& trainCode, 
+                        int carriageNo, int seatNo, int newFromID, int newToID) const;
+    void displaySeatMap(StationList* stationList, const std::string& trainCode,
+                        int carriageNo, int totalSeats, int fromID, int toID) const;
     void bookTicketUI(StationList* stationList, TrainList* trainList);
 };
